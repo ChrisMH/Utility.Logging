9dc9bad5b096208da86c120fb3effcbd178c9478
Chris Hogan <chogan@logikos.com>
2011-11-14 14:09:06 -0500

    Removed tools, updated PowerShell scripts

--- NugetPack.ps1
+++ NugetPack.ps1
@@ -1,44 +1,14 @@
 $srcRoot = '.\src'                     # relative to script directory
 $versionFile = 'SharedAssemblyInfo.cs' # relative to $srcRoot
 $outputPath = '.\build'                # relative to script directory
-$nugetCmd = '.\tools\nuget\nuget.exe'  # relative to script directory
+$scriptRoot = "$home\Dropbox\Scripts"
 
-function Get-Version($srcRoot, $versionFile)
-{
-  $versionFile = Join-Path $srcRoot $versionFile -Resolve
-  Get-Content $versionFile | Where-Object { $_ -match '^\[\s*assembly:\s*AssemblyVersion\s*\(\s*\"(?<version>[\d\.]*)\"\s*\)\s*\]' } | out-null
-  return $matches.version
-}
+. "$scriptRoot\New-Path.ps1" $outputPath
 
-function Pack-Project($packageName, $srcRoot, $version, $outputPath, $nugetCmd)
-{
-  "`nBuilding Package : $packageName"
-  $projectRoot = Join-Path $srcRoot $packageName -Resolve
-  $packFile = Join-Path $projectRoot "$packageName.csproj" -Resolve
-  &$nugetCmd pack $packFile -Version $version -Build -Properties Configuration=Release -OutputDirectory $outputPath
-}
+$version = . "$home\Dropbox\Scripts\Get-Version.ps1" (Join-Path $srcRoot $versionFile -Resolve)
 
-function Pack-ContentProject($packageName, $srcRoot, $version, $outputPath, $nugetCmd)
-{
-  "`nBuilding Content Package : $packageName"
-  $projectRoot = Join-Path $srcRoot $packageName -Resolve
-  $packFile = Join-Path $projectRoot "$packageName.nuspec" -Resolve
+. "$scriptRoot\Pack-Project.ps1" Utility.Logging $srcRoot $version $outputPath
+. "$scriptRoot\Pack-Project.ps1" Utility.Logging.NLog $srcRoot $version $outputPath
+. "$scriptRoot\Pack-ContentProject.ps1" Utility.Logging.NLog.Autofac $srcRoot $version $outputPath
+. "$scriptRoot\Pack-ContentProject.ps1" Utility.Logging.NLog.Ninject $srcRoot $version $outputPath
 
-  #Remove binary directories so that they don't get included in the nupkg.  This is a content-only package.
-  $binPath = "$projectRoot\bin"
-  if (Test-Path $binPath) { Remove-Item $binPath -Recurse -Force | Out-Null }
-  $objPath = "$projectRoot\obj"
-  if (Test-Path $objPath) { Remove-Item $objPat -Recurse -Force | Out-Null }
-
-  &$nugetCmd pack $packFile -Version $version -OutputDirectory $outputPath
-}
-
-$outputPath = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($outputPath)
-if((Test-Path $outputPath) -eq $False) { New-Item -Path $outputPath -ItemType directory }
-
-$version = Get-Version $srcRoot $versionFile
-
-Pack-Project "Utility.Logging" $srcRoot $version $outputPath $nugetCmd
-Pack-project "Utility.Logging.NLog" $srcRoot $version $outputPath $nugetCmd
-Pack-ContentProject "Utility.Logging.NLog.Autofac" $srcRoot $version $outputPath $nugetCmd
-Pack-ContentProject "Utility.Logging.NLog.Ninject" $srcRoot $version $outputPath $nugetCmd
--- NugetPush.ps1
+++ NugetPush.ps1
@@ -1,27 +1,11 @@
 $srcRoot = '.\src'                     # relative to script directory
 $versionFile = 'SharedAssemblyInfo.cs' # relative to $srcRoot
 $outputPath = '.\build'                # relative to script directory
-$nugetCmd = '.\tools\nuget\nuget.exe'  # relative to script directory
+$scriptRoot = "$home\Dropbox\Scripts"
 
-function Get-Version($srcRoot, $versionFile)
-{
-  $versionFile = Join-Path $srcRoot $versionFile -Resolve
-  Get-Content $versionFile | Where-Object { $_ -match '^\[\s*assembly:\s*AssemblyVersion\s*\(\s*\"(?<version>[\d\.]*)\"\s*\)\s*\]' } | out-null
-  return $matches.version
-}
+$version = . "$home\Dropbox\Scripts\Get-Version.ps1" (Join-Path $srcRoot $versionFile -Resolve)
 
-function Push-Project($packageName, $srcRoot, $version, $outputPath, $nugetCmd)
-{
-  "`nPushing Package : $packageName"
-  $pushFile = Join-Path $outputPath "$packageName.$version.nupkg" -Resolve
-  &$nugetCmd delete $packageName $version -NoPrompt
-  &$nugetCmd push $pushFile
-}
-
-
-$version = Get-Version $srcRoot $versionFile
-
-Push-Project "Utility.Logging" $srcRoot $version $outputPath $nugetCmd
-Push-Project "Utility.Logging.NLog" $srcRoot $version $outputPath $nugetCmd
-Push-Project "Utility.Logging.NLog.Autofac" $srcRoot $version $outputPath $nugetCmd
-Push-Project "Utility.Logging.NLog.Ninject" $srcRoot $version $outputPath $nugetCmd
+. "$scriptRoot\Push-Project.ps1" Utility.Logging $srcRoot $version $outputPath
+. "$scriptRoot\Push-Project.ps1" Utility.Logging.NLog $srcRoot $version $outputPath
+. "$scriptRoot\Push-Project.ps1" Utility.Logging.NLog.Autofac $srcRoot $version $outputPath
+. "$scriptRoot\Push-Project.ps1" Utility.Logging.NLog.Ninject $srcRoot $version $outputPath
