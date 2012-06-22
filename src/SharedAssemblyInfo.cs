47dcb2a0ccbee696c594b1a71c06a50d9586aab8
Chris Hogan <hogancm@gmail.com>
2012-06-22 10:48:05 -0400

    Updated Autofac to 2.6.3, Removed version requirements from nuspec dependencies

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.4.2.2")]
-[assembly: AssemblyFileVersion("1.4.2.2")]
+[assembly: AssemblyVersion("1.4.2.3")]
+[assembly: AssemblyFileVersion("1.4.2.3")]
