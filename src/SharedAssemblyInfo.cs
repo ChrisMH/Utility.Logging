12320c9c8fd20a875f545a390813f2d4bd791e40
ArthurLin <arthurlin@91app.com>
2021-04-13 11:56:12 +0800

    Config - update Shared Assembly Info

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -9,6 +9,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.4.2.3")]
-[assembly: AssemblyFileVersion("1.4.2.3")]
-[assembly: AssemblyInformationalVersion("1.4.2.3")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0.0")]
